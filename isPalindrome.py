f6dc4d1d7276c0683be95b89854be1e0f66a4aad
Bakhtiar Ahmed <114367140+BakhtiarAhmed41@users.noreply.github.com>
2025-05-29 00:32:11 +0500

    Update isPalindrome.py

--- isPalindrome.py
+++ isPalindrome.py
@@ -1,7 +1,26 @@
 class Solution(object):
     def isPalindrome(self, x):
-        """
-        :type x: int
-        :rtype: bool
-        """
-        
+        num = str(x)
+        arr = []
+
+        if(len(num)==1):
+            return True
+        else:
+            for i in range(len(num)):
+                arr.append(num[i])
+            
+            for i in range(len(arr)/2):
+
+                
+                if(i == len(arr)/2 - 1):
+                    if(arr[i] == arr[len(arr)-i-1]):
+                        return True
+                    else:
+                        return False
+
+                elif(arr[i] == arr[len(arr)-i-1]):
+                    continue
+
+                else:
+                    return False    
+            
