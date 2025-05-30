76553033d2a7b89161ebf494c9c594b42f2a90f5
Bakhtiar Ahmed <114367140+BakhtiarAhmed41@users.noreply.github.com>
2025-05-31 01:21:30 +0500

    Update mergeTwoLists.py

--- mergeTwoLists.py
+++ mergeTwoLists.py
@@ -6,9 +6,29 @@
 
 class Solution(object):
     def mergeTwoLists(self, list1, list2):
-        """
-        :type list1: Optional[ListNode]
-        :type list2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
+        nodes = []
+
+        while list1:
+            nodes.append(list1.val)
+            list1 = list1.next
+
+        while list2:
+            nodes.append(list2.val)
+            list2 = list2.next
+
+        if not nodes:
+            return None
+
+        nodes.sort()
+
+        head = ListNode(nodes[0])
+        current = head
+        for val in nodes[1:]:
+            current.next = ListNode(val)
+            current = current.next
+
+        return head
+
+                
+
         
