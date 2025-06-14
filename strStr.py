5e4d1b8cf3e86844e81ac2c184d4abf1a4b7e71a
Bakhtiar Ahmed <114367140+BakhtiarAhmed41@users.noreply.github.com>
2025-06-15 00:33:08 +0500

    Update strStr.py

--- strStr.py
+++ strStr.py
@@ -2,11 +2,7 @@ class Solution(object):
     def strStr(self, haystack, needle):
         
         if needle in haystack:
-            for i in range(0, len(haystack)):
-                if(i != len(haystack)-1):
-                    if ((haystack[i] == needle[0]) and (haystack[i+1] == needle[1])):
-                        return i
-                    else:
-                        continue
+            return haystack.find(needle)
+        
         else:
             return -1
