c9a1ee10b461bdb4cb60d5116956c3aa13542df7
Bakhtiar Ahmed <114367140+BakhtiarAhmed41@users.noreply.github.com>
2025-07-01 23:52:55 +0500

    Create addBinary.py

---
+++ addBinary.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def addBinary(self, a, b):
+        
+        sum = int(a , 2) + int(b , 2)
+        sum = str(bin(sum))
+
+        
+        return sum
+        
+        
