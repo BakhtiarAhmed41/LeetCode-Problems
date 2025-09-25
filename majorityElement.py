04aff03b56ccf2bde3e2f37a44d96cf3f9cb0f65
Bakhtiar Ahmed <ahmedbakhtiar41@gmail.com>
2025-09-25 16:26:12 +0500

    Update majorityElement.py

--- majorityElement.py
+++ majorityElement.py
@@ -1,5 +1,11 @@
 class Solution(object):
     def majorityElement(self, nums):
-        
-        return max(nums, key=nums.count)
-        
+        count = 0
+        candidate = None
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += (1 if num == candidate else -1)
+
+        return candidate
