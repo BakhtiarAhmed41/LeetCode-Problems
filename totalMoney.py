4bfacc029bb6c721c649c95f097f18d27d562e26
Bakhtiar Ahmed <ahmedbakhtiar41@gmail.com>
2025-10-26 00:00:19 +0500

    Update totalMoney.py

--- totalMoney.py
+++ totalMoney.py
@@ -1,9 +1,17 @@
 class Solution(object):
     def totalMoney(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
+        add = 0
+        sum = 0
+        for i in range(n):
+
+            if (i%7==0):
+                add = (i / 7) +1
+                sum = sum + add
+            
+            else:    
+                add = add+1
+                sum = sum + add
+
+        return sum
         
 
-        
