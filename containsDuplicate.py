c03ff404be9e77124cdbef1a1589a023bea01797
Bakhtiar Ahmed <ahmedbakhtiar41@gmail.com>
2025-10-28 03:24:17 +0500

    Update containsDuplicate.py

--- containsDuplicate.py
+++ containsDuplicate.py
@@ -1,17 +1,5 @@
 class Solution(object):
     def containsDuplicate(self, nums):
         
-        for i in range(len(nums)):
+        return len(nums) != len(set(nums))
 
-            if count(nums[i]) > 1:
-                return True
-            continue
-        return False
-
-
-        #     for j in range(i+1, len(nums)):
-        #         if nums[i] == nums[j]:
-        #             return True
-            
-        # return False
-        
