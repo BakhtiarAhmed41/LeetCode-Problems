909580607a44c17ed5ecdf422f6e6f4e94ad4cf8
Bakhtiar Ahmed <ahmedbakhtiar41@gmail.com>
2025-11-15 00:18:20 +0500

    Update isAnagram.py

--- isAnagram.py
+++ isAnagram.py
@@ -1,8 +1,4 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
         
+        return sorted(s) == sorted(t)
