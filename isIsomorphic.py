b55ead540e35cc32c38d613915eceb4f419f0530
Bakhtiar Ahmed <ahmedbakhtiar41@gmail.com>
2025-11-17 00:16:01 +0500

    Implement isIsomorphic function with mapping checks

--- isIsomorphic.py
+++ isIsomorphic.py
@@ -1,5 +1,27 @@
 class Solution(object):
     def isIsomorphic(self, s, t):
         
+        if len(s) != len(t):
+            return False
         
+        mapST = {}  # mapping from s → t
+        mapTS = {}  # mapping from t → s
         
+        for i in range(len(s)):
+            ch1, ch2 = s[i], t[i]
+            
+            # If mapping exists, check consistency
+            if ch1 in mapST:
+                if mapST[ch1] != ch2:
+                    return False
+            else:
+                mapST[ch1] = ch2
+            
+            # Also check reverse mapping
+            if ch2 in mapTS:
+                if mapTS[ch2] != ch1:
+                    return False
+            else:
+                mapTS[ch2] = ch1
+        
+        return True
