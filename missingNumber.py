bb79691014ab3195d9837c79ee90c6d20cad8962
Bakhtiar Ahmed <ahmedbakhtiar41@gmail.com>
2025-11-22 00:12:09 +0500

    Update missingNumber.py

--- missingNumber.py
+++ missingNumber.py
@@ -2,11 +2,12 @@ class Solution(object):
     def missingNumber(self, nums):
         
         for i in range(0,len(nums)+1):
-            sorted(nums)
+            nums = sorted(nums)
 
             if i == len(nums):
                 return i
 
-            elif i != nums[i]:
-                return i
+            else:
+                if i != nums[i]:
+                    return i
         
