7b3f2a6978482b0b14da34200ea8e434c8bac0d8
Bakhtiar Ahmed <ahmedbakhtiar41@gmail.com>
2025-12-16 00:28:54 +0500

    Update summaryRanges.py

--- summaryRanges.py
+++ summaryRanges.py
@@ -4,4 +4,27 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[str]
         """
-        
+        if not nums:
+            return []
+
+        result = []
+        start = nums[0]
+
+        for i in range(1, len(nums)):
+           
+            if nums[i] != nums[i - 1] + 1:
+                end = nums[i - 1]
+                if start == end:
+                    result.append(str(start))
+                else:
+                    result.append(str(start) + "->" + str(end))
+                start = nums[i]
+
+   
+        end = nums[-1]
+        if start == end:
+            result.append(str(start))
+        else:
+            result.append(str(start) + "->" + str(end))
+
+        return result
