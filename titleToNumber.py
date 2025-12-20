c9561961a3b31e28e99421c7de205416262c8d1f
Bakhtiar Ahmed <ahmedbakhtiar41@gmail.com>
2025-12-21 00:12:50 +0500

    Update titleToNumber.py

--- titleToNumber.py
+++ titleToNumber.py
@@ -1,7 +1,13 @@
 class Solution(object):
     def titleToNumber(self, columnTitle):
-        """
-        :type columnTitle: str
-        :rtype: int
-        """
-        
+        x = [
+            'A','B','C','D','E','F','G','H','I','J',
+            'K','L','M','N','O','P','Q','R','S','T',
+            'U','V','W','X','Y','Z'
+        ]
+
+        y = 0
+        for char in columnTitle:
+            y = y * 26 + (x.index(char) + 1)
+
+        return y
