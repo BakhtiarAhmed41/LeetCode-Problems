2b72b1287722ffa46eab4a25a2df6fe4be1bb202
Bakhtiar Ahmed <ahmedbakhtiar41@gmail.com>
2026-01-01 00:07:22 +0500

    Update findRelativeRanks.py

--- findRelativeRanks.py
+++ findRelativeRanks.py
@@ -1,7 +1,7 @@
 class Solution(object):
     def findRelativeRanks(self, score):
         rank = []    
-        sort = sorted(score)
+        sort = sorted(score, reverse=True)
 
         for i in range(len(score)):
             
@@ -15,6 +15,6 @@ class Solution(object):
                 rank.append("Bronze Medal")
             
             else:
-                rank.append(str(sort.index(score[i]) + 1) + "th")
+                rank.append(str(sort.index(score[i]) + 1))
         return rank
         
