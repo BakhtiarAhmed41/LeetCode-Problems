8f8d5402d5c2006a10c29ea583307e71fdccdcb8
Bakhtiar Ahmed <ahmedbakhtiar41@gmail.com>
2026-01-09 00:00:13 +0500

    Update moveZeroes.py

--- moveZeroes.py
+++ moveZeroes.py
@@ -1,7 +1,3 @@
 class Solution(object):
     def moveZeroes(self, nums):
-        
-        :type nums: List[int]
-        :rtype: None Do not return anything, modify nums in-place instead.
-        """
-        
+    
