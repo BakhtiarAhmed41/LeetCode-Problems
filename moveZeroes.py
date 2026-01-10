5d89a0ef5ad51ffedcdb76f5d95dba0dffe8a32e
Bakhtiar Ahmed <ahmedbakhtiar41@gmail.com>
2026-01-10 23:57:18 +0500

    Update moveZeroes.py

--- moveZeroes.py
+++ moveZeroes.py
@@ -1,3 +1,17 @@
 class Solution(object):
     def moveZeroes(self, nums):
+        
+        nums2 = []
+        nums2 = nums
+
+        nums2.sort()
+        nums.remove(0)
+
+        for i in nums2:
+            if i == 0:
+                nums.append(i)
+
+
+        return nums
+
     
